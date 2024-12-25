71402c780412664a3b3165401ac2d20338c12c31
BasitSol <sigmabphile1052@gmail.com>
2024-12-25 14:01:57 +0000

    Datatypes Done

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // This Would Treat all the JavaScript code as newer version
+//alert(3+3) // we are using node js not browser
+
+console.log(3 + 3)
+
+let name = " Basit"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false 
+// null => Standalone value
+// undefined =>
+// symbol => used for the uniqueness
+// object 
+
+console.log(typeof undefined); // undefined is the Type in JavaScript
+console.log(null); // Null is the object => interview question
+
+
+
+
+
+
