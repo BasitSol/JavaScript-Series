6c3df8fccbc3bc6ef080002c6e3ace47e0f12738
BasitSol <sigmabphile1052@gmail.com>
2024-12-25 14:35:57 +0000

    Conversion of data types

---
+++ 01_Basics/conversionOperations.js
@@ -0,0 +1,35 @@
+let score = undefined
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); // Now it will give the NAN 
+
+
+// Notes: that Converts
+// "33" => 33
+// "33abc" => NaN but its type is Number
+// true => 1 ; false => 0
+
+let isLoggedIn = 1
+//let isLoggedIn = "" // This will print a false for the empty strings
+//let isLoggedIn = "Basit" // This will print true for the non empty string
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+
