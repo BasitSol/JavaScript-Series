ab8c67af724724d76fa379c8957c3d7bcf73b612
BasitSol <sigmabphile1052@gmail.com>
2024-12-26 17:29:32 +0000

    Comparisons operators

---
+++ 01_Basics/comparisons.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log( 2 == 1); // this is simple
+
+console.log("2" > 1); // print true coz JS automaatically convert into number
+// // but keep the datatypes of both as same its good to have that practise
+
+console.log(null > 0); // false
+console.log(null == 0); // False 
+console.log(null >= 0); // True coz it dont shows the predictable results
+
+// In Js the working of (==) and (>=) works differently 
+// for (==) => It doesnt converts null into 0
+// but for (>=) => it does converts null into 0 or sometimes NaN
+// Same goes for the Undefined 
+
+//************************NEW Comparison Option************* */ */
+
+// (===) => this will strictly check the datatypes as well and give results based in that 
+console.log("2" === 2); // this will print False
+
+
+
+
+
+
+
+
+
