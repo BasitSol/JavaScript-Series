b44c7a46012e428de89a4031d85ad779ca66422f
BasitSol <sigmabphile1052@gmail.com>
2024-12-26 15:37:50 +0000

    Conversions and operations

--- 01_Basics/conversionOperations.js
+++ 01_Basics/conversionOperations.js
@@ -1,8 +1,8 @@
-let score = undefined
+// let score = undefined
 //console.log(typeof score);
 //console.log(typeof(score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber); // Now it will give the NAN 
 
@@ -12,17 +12,68 @@ let valueInNumber = Number(score)
 // "33abc" => NaN but its type is Number
 // true => 1 ; false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 //let isLoggedIn = "" // This will print a false for the empty strings
 //let isLoggedIn = "Basit" // This will print true for the non empty string
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//******************************* Operations*****************************//
+// let value = 3
+// let negValue = -value
+// console.log(negValue); //This will give the negative Value
+
+// console.log(2+2);
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2/1);
+
+// let str1 = " hello"
+// let str2 = "basit"
+// let str3 = str1 + str2 // This will combine the two strings 
+// console.log(str3);
+
+// console.log("1" + 2);  // this will print 12 like concatenated
+// console.log(1 + "2"); // This will also print 12 side by side 
+// console.log("1" + 2 + 2); // This will print 122
+// console.log(1 + 2 + " 2"); // This will print 32
+
+// This is because in javascript if the string is first then it will print further more as string
+// If string is last then the previous operations would run first 
+
+// console.log(true); // This will print true
+// console.log(+true); // This will print 1
+
+// let gameCounter = 100;
+// gameCounter++
+// ++gameCounter
+// console.log(gameCounter);
+
+let x = 8;
+let y = x++
+console.log(y);
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
